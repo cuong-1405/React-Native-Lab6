0ae0722aca558c5b11e47bd16e6081c564a123f1
chekhucbach <75738760+cuong-1405@users.noreply.github.com>
2023-10-06 15:54:40 +0700

    update

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MyPicker />;
 }
 
+const MyPicker = () => {
+  const [selectedValue, setSelectedValue] = useState("java"); // Giá trị mặc định
+  const thongBao = (itemValue) => {
+    setSelectedValue(itemValue);
+    // Alert.alert("Bạn đã chọn", itemValue);
+    Alert.alert("Bạn đã chọn ", itemValue);
+  };
+  return (
+    <View style={styles.container}>
+      <Picker
+        selectedValue={selectedValue}
+        onValueChange={(itemValue, itemIndex) => thongBao(itemValue)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="JavaScript" />
+        <Picker.Item label="Python" value="python" />
+        <Picker.Item label="C#" value="csharp" />
+      </Picker>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  picker: {
+    backgroundColor: "aqua",
+    width: 400, // Điều chỉnh độ rộng của Picker tùy ý
   },
 });
